c6827bbeb8d7b82bce383b977b4ba0fbe9e1f3cd
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-26 15:38:29 +0530

    Added : Ability To unpark vehicle

--- src/main/java/com/bridgelabz/parkinglot/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/parkinglot/ParkingLotSystem.java
@@ -1,9 +1,12 @@
 package com.bridgelabz.parkinglot;
 
 public class ParkingLotSystem {
+    private Object vehicle;
+
     public ParkingLotSystem() {
 
     }
+
     /**
      * @Purpose : To park vehicle
      * @Param : obj vehicle
@@ -11,6 +14,23 @@ public class ParkingLotSystem {
      * @Return :boolean t/f
      */
     public boolean park(Object vehicle) {
+        if (this.vehicle != null)
+            return false;
+        this.vehicle = vehicle;
         return true;
+    }
+    /**
+     * @Purpose : To un park vehicle
+     * @Param : obj vehicle
+     * @Function :To unpark vehicle and check status 
+     * @Return :boolean t/f
+     */
+    public boolean unPark(Object vehicle) {
+        if (vehicle == null) return false;
+        if (this.vehicle.equals(vehicle)) {
+            this.vehicle = null;
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/com/bridgelabz/parkinglot/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/parkinglot/ParkingLotTest.java
@@ -1,16 +1,47 @@
 package com.bridgelabz.parkinglot;
 
+import org.junit.Before;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class ParkingLotTest {
+    ParkingLotSystem parkingLotSystem = null;
+    Object vehicle = null;
+
+    @Before
+    public void setUp() throws Exception {
+        vehicle = new Object();
+        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
+
+
+    }
+
     /**
      * @Purpose : Test for parking vehicle UC1 .
      */
+
     @Test
     public void givenVehicleWhenParked_ShouldReturnTrue() {
-        ParkingLotSystem parkingLotSystem = new ParkingLotSystem();
-      boolean isParked =  parkingLotSystem.park(new Object());
-      Assert.assertTrue(isParked);
+        parkingLotSystem.park(vehicle);
+        boolean isParked = parkingLotSystem.park(new Object());
+        Assert.assertTrue(isParked);
+    }
+    /**
+     * @Purpose : Test for Unpark vehicle UC2 .
+     */
+    @Test
+    public void givenVehicleWhenUnparked_ShouldReturnTrue() {
+        parkingLotSystem.park(vehicle);
+        boolean isUnparked = parkingLotSystem.unPark(new Object());
+        Assert.assertTrue(isUnparked);
+    }
+    /**
+     * @Purpose : Test for previously parked vehicle .
+     */
+    @Test
+    public void givenVehicleWhenAlreadyParked_ShouldReturnFalse() {
+        boolean isParked = parkingLotSystem.park(new Object());
+        Assert.assertFalse(isParked);
     }
 }
